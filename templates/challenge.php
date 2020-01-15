b6517283fd62912f705810ec635d594672b37160
Cédric Laubacher <cedric@laubacher.io>
2020-01-15 15:13:23 +0100

    Update challenge.php

--- templates/challenge.php
+++ templates/challenge.php
@@ -1,5 +1,4 @@
 <form method="POST">
 	<input type="hidden" name="redirect_url" value="<?php p($_['redirect_url']); ?>">
-	<input type="text" name="challenge" autocomplete="off" autocapitalize="off" value="passme" required="required">
-	<input type="submit" class="button" value="Verify">
+	<input type="submit" name="challenge" autocomplete="off" autocapitalize="off" value="AGB akzeptieren" required="required">
 </form>
